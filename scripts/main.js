322088a43d6af63edb3a9700c2c855b534032411
PatrickBoynton <jpboynton3@gmail.com>
2021-02-03 13:04:42 -0500

    Created HTML via Handlebars.

--- scripts/main.js
+++ scripts/main.js
@@ -8,12 +8,17 @@
         const userNameText = document.querySelector("#username");
 
         const BASE_URL = "https://tiny-taco-server.herokuapp.com/api/v1/chats/";
-
+        const generateHTML = (data) => {
+            const source = document.querySelector("#create-message").innerHTML;
+            const template = Handlebars.compile(source);
+            const context = {todos: data};
+            const html = template(context);
+            console.log(data);
+            document.querySelector("#user-input").innerHTML = html;
+        }
         fetch(BASE_URL)
             .then(response => response.json())
-            .then(data => console.log(data))
+            .then(data => generateHTML(data))
             .catch(error => console.log(error));
-
-
     }
 )();
