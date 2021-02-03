8ade24ffcd08352a294f00e99ffaa84ef0e6bbab
PatrickBoynton <jpboynton3@gmail.com>
2021-02-03 18:55:56 -0500

    Created a delete listener.

--- scripts/main.js
+++ scripts/main.js
@@ -7,6 +7,8 @@
         const messageArea = document.querySelector("#message");
         const userNameText = document.querySelector("#username");
         const form = document.querySelector("form");
+        const deleteButton = document.querySelector(".delete");
+        const hidden = document.querySelector("#chat-id");
         const BASE_URL = "https://tiny-taco-server.herokuapp.com/api/v1/chats/";
         const chats = {
             text: "",
@@ -17,9 +19,24 @@
             const template = Handlebars.compile(source);
             const context = {chats: data};
             const html = template(context);
-            console.log(data);
+            console.log(context);
             document.querySelector("#user-input").innerHTML = html;
         }
+        document.querySelector("ul").addEventListener("click", (e) =>{
+            // e.target.parentNode.remove();
+                if (e.target.classList.contains("delete")) {
+                    console.dir(e.target.value);
+                }
+            fetch(`${BASE_URL}${e.target.value}`, {
+                method: "DELETE",
+                headers: {
+                    "Content-Type":"application/json"
+                },
+                // body: JSON.stringify(chats)
+            })
+                .then(response => response.json())
+                .then(data => console.log("Data", data));
+        });
         document.querySelector("form").addEventListener("submit", (e) => {
             chats.text = document.querySelector("textarea").value;
             chats.username = document.querySelector("input").value;
