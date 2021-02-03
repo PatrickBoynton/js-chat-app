5030a73d2362ac081a94430342b000c7ca4d63f4
PatrickBoynton <jpboynton3@gmail.com>
2021-02-03 11:57:27 -0500

    Added selectors.

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,10 @@
 (function () {
         "use strict";
-        console.log("Test");
+        const messageText = document.querySelector(".message");
+        const userName = document.querySelector(".username");
+        const deleteMessage = document.querySelector(".delete");
+        const edit = document.querySelector(".edit");
+        const messageArea = document.querySelector("#message");
+        const userNameText = document.querySelector("#username");
     }
 )();
