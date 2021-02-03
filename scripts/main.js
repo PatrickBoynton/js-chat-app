9d083f8bd570650f3699d79b097266f497e740b2
PatrickBoynton <jpboynton3@gmail.com>
2021-02-03 12:03:01 -0500

    Added the base url.

--- scripts/main.js
+++ scripts/main.js
@@ -6,5 +6,9 @@
         const edit = document.querySelector(".edit");
         const messageArea = document.querySelector("#message");
         const userNameText = document.querySelector("#username");
+
+        const BASE_URL = "https://tiny-taco-server.herokuapp.com/api/v1/chats/";
+
+
     }
 )();
