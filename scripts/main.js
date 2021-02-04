0b407883940076909bd6f1775a31fad2de24a712
PatrickBoynton <jpboynton3@gmail.com>
2021-02-03 20:42:11 -0500

    Removed the querySelectors. Added a set interval.

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,7 @@
 (function () {
         "use strict";
         const messageText = document.querySelector("#message");
-        const userName = document.querySelector(".username");
-        const deleteMessage = document.querySelector(".delete");
-        const edit = document.querySelector(".edit");
-        const messageArea = document.querySelector("#message");
         const userNameText = document.querySelector("#username");
-        const form = document.querySelector("form");
-        const deleteButton = document.querySelector(".delete");
-        const hidden = document.querySelector("#chat-id");
         const BASE_URL = "https://tiny-taco-server.herokuapp.com/api/v1/chats/";
         const chats = {
             text: "",
@@ -17,49 +10,68 @@
         const generateHTML = (data) => {
             const source = document.querySelector("#create-message").innerHTML;
             const template = Handlebars.compile(source);
-            const context = {chats: data};
+            const context = {chats: data.sort()};
             const html = template(context);
             console.log(context);
             document.querySelector("#user-input").innerHTML = html;
         }
-        document.querySelector("ul").addEventListener("click", (e) =>{
+        document.querySelector("ul").addEventListener("click", (e) => {
             // e.target.parentNode.remove();
-                if (e.target.classList.contains("delete")) {
-                    console.dir(e.target.value);
-                }
-            fetch(`${BASE_URL}${e.target.value}`, {
-                method: "DELETE",
-                headers: {
-                    "Content-Type":"application/json"
-                },
-                // body: JSON.stringify(chats)
-            })
-                .then(response => response.json())
-                .then(data => console.log("Data", data));
+            if (e.target.classList.contains("delete")) {
+                console.dir(e.target.value);
+                fetch(`${BASE_URL}${e.target.value}`, {
+                    method: "DELETE",
+                    headers: {
+                        "Content-Type": "application/json"
+                    },
+                    // body: JSON.stringify(chats)
+                })
+                    .then(response => response.json())
+                    .then(data => console.log("Data", data));
+            }
+        });
+
+        document.querySelector("ul").addEventListener("click", (e) => {
+            if (e.target.classList.contains("edit")) {
+                const message = document.querySelector(".message");
+                const user = document.querySelector(".username");
+                messageText.value = message.innerHTML;
+                userNameText.value = user.innerHTML;
+                fetch(`${BASE_URL}${e.target.value}`, {
+                    method: "PUT",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+
+                })
+            }
+
         });
         document.querySelector("form").addEventListener("submit", (e) => {
             chats.text = document.querySelector("textarea").value;
             chats.username = document.querySelector("input").value;
 
             fetch(BASE_URL, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(chats),
-        })
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error("Something went wrong.");
-                }
-                return response.json();
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(chats),
             })
-            .then(data => consol.log(data))
-            .catch(error => console.log("Error:", error));
+                .then(response => {
+                    if (!response.ok) {
+                        throw new Error("Something went wrong.");
+                    }
+                    return response.json();
+                })
+                .then(data => console.log(data))
+                .catch(error => console.log("Error:", error));
         });
-        fetch(BASE_URL)
+        setInterval(() => {
+            fetch(BASE_URL)
             .then(response => response.json())
-            .then(data => generateHTML(data))
+            .then(data => generateHTML(data.sort()))
             .catch(error => console.log(error));
+        }, 3000);
     }
 )();
